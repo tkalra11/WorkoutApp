02dd213262d81b693352b1b12505138a4d8e6128
tkalra11 <tkalra1111@gmail.com>
2026-02-06 09:52:40 +0530

    firebase func 3

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,13 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.8.0/firebase-firestore.js";
 
 const firebaseConfig = {
-        apiKey: "FIREBASE_API_KEY",
-        authDomain: "FIREBASE_AUTH_DOMAIN",
-        projectId: "FIREBASE_PROJECT_ID",
-        storageBucket: "FIREBASE_STORAGE_BUCKET",
-        messagingSenderId: "FIREBASE_MESSAGING_SENDER_ID",
-        appId: "FIREBASE_APP_ID",
-        measurementId: "FIREBASE_MEASUREMENT_ID"
+        apiKey: "$FIREBASE_API_KEY",
+        authDomain: "$FIREBASE_AUTH_DOMAIN",
+        projectId: "$FIREBASE_PROJECT_ID",
+        storageBucket: "$FIREBASE_STORAGE_BUCKET",
+        messagingSenderId: "$FIREBASE_MESSAGING_SENDER_ID",
+        appId: "$FIREBASE_APP_ID",
+        measurementId: "$FIREBASE_MEASUREMENT_ID"
 };
 
 const app = initializeApp(firebaseConfig);
